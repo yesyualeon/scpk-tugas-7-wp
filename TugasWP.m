56e4eee97eee4770c285a8c744c318e9490a8469
yesyualeon <54468793+yesyualeon@users.noreply.github.com>
2020-04-27 09:44:59 +0700

    Update TugasWP.m

--- TugasWP.m
+++ TugasWP.m
@@ -3,15 +3,16 @@
 % Kriteria : Nilai Rata-rata UN, Nilai Rata-rata Rapot Semester 1- 5, Prestasi Akademik, Prestasi
 % Non Akademik, Ekstrakurikuler, Sikap, Presensi, Pelanggaran
 
-%Data       C1  C2          C3          C4      C5          C6      C7   C8
-%Adela		90	95			20			10		4.00	    4.00	895  1
-%Devita		91	95			20			10		4.00	    4.00	895	 1
-%Sukma		89	92			50			30		4.00	    4.00	894	 1
-%Lisa		93	93			30			10		3.00	    4.00	894	 1
-%Iqbal		88	90			60			10		3.00	    4.00	892	 1
-%Langit		85	85			20			10		3.00	    4.00	900	 2
-%Febi		84	86			20			10		3.00	    4.00	900	 1
-%Idam		87	87			30			50		4.00	    4.00	895	 1
+%Data       C1  C2          C3          C4      C5          C6      C7   C8      C9
+%Adela		90	95			20			10		4.00	    4.00	895  1          3.50
+%Devita		91	95			20			10		4.00	    4.00	895	 1        4.00
+%Sukma		89	92			50			30		4.00	    4.00	894	 1        3.50
+%Lisa		93	93			30			10		3.00	    4.00	894	 1           3.45
+%Iqbal		88	90			60			10		3.00	    4.00	892	 1        3.67
+%Langit		85	85			20			10		3.00	    4.00	900	 2        3.20
+%Febi		84	86			20			10		3.00	    4.00	900	 1           3.35
+%Idam		87	87			30			50		4.00	    4.00	895	 1           3.87
+%Kevin      87  93        30    10      3.50        4.00    893  1    3.95
 
 % Kriteria      Sifat       Bobot
 %    C1         max 1        4
@@ -22,6 +23,7 @@
 %    C6         max 1        3
 %    C7         max 1        2
 %    C8         min 0        4
+%    C9         max 1        3
 
 m = input('Masukkan jumlah alternatif : ');
 n = input('Masukkan jumlah kriteria : ');
